75c9ba559544b6ad837439a90c100215a0d508c7
moonstal1506 <moonstal1506@gmail.com>
2023-12-19 01:11:54 +0900

    JobRepository

--- src/main/java/io/springbatch/springbatchlecture/JobRepositoryConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobRepositoryConfiguration.java
@@ -2,6 +2,7 @@ package io.springbatch.springbatchlecture;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -10,51 +11,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
-//@Configuration
-public class JobConfiguration {
+@Configuration
+public class JobRepositoryConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-    private final ExecutionContextTasklet1 executionContextTasklet1;
-    private final ExecutionContextTasklet2 executionContextTasklet2;
-    private final ExecutionContextTasklet3 executionContextTasklet3;
-    private final ExecutionContextTasklet4 executionContextTasklet4;
+    private final JobExecutionListener jobRepositoryListener;
 
     @Bean
     public Job job() { //SimpleJob(설계도)
-        return jobBuilderFactory.get("job")
+        return jobBuilderFactory.get("batchJob")
                 .start(step1())
                 .next(step2())
-                .next(step3())
-                .next(step4())
+                .listener(jobRepositoryListener)
                 .build(); //job에 2개의 step을 저장 -> jobLuncher가 실행
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(executionContextTasklet1)
+                .tasklet((stepContribution, chunkContext) -> RepeatStatus.FINISHED)
                 .build();
     }
 
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(executionContextTasklet2)
-                .build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet(executionContextTasklet3)
-                .build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .tasklet(executionContextTasklet4)
+                .tasklet((stepContribution, chunkContext) -> RepeatStatus.FINISHED)
                 .build();
     }
 
