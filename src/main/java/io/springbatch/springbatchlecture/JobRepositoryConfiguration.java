3a6b45506eae12b18ecfcf6811abfb01365aa048
moonstal1506 <moonstal1506@gmail.com>
2023-12-20 20:57:21 +0900

    JobLauncher

--- src/main/java/io/springbatch/springbatchlecture/JobRepositoryConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobRepositoryConfiguration.java
@@ -2,7 +2,6 @@ package io.springbatch.springbatchlecture;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -16,21 +15,27 @@ public class JobRepositoryConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-    private final JobExecutionListener jobRepositoryListener;
+//    private final JobExecutionListener jobRepositoryListener;
 
     @Bean
     public Job job() { //SimpleJob(설계도)
         return jobBuilderFactory.get("batchJob")
                 .start(step1())
                 .next(step2())
-                .listener(jobRepositoryListener)
+//                .listener(jobRepositoryListener)
                 .build(); //job에 2개의 step을 저장 -> jobLuncher가 실행
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet((stepContribution, chunkContext) -> RepeatStatus.FINISHED)
+                .tasklet((stepContribution, chunkContext) -> {
+                            // 동기적 실행: 모든 것이 끝나고 job execution 객체 반환
+                            // 비동기적 방식: 클라이언트가 잡을 수행 요청 -> 일단 반환해 -> 그 다음 내부적으로 로직 실행
+                            Thread.sleep(3000);
+                            return RepeatStatus.FINISHED;
+                        }
+                )
                 .build();
     }
 
--- src/main/java/io/springbatch/springbatchlecture/JobRepositoryListener.java
+++ src/main/java/io/springbatch/springbatchlecture/JobRepositoryListener.java
@@ -5,7 +5,7 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class JobRepositoryListener implements JobExecutionListener {
 
     @Autowired
