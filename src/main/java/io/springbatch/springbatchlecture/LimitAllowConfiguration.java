1bcd34a479295bfb53c55bf7069d041d95970c0e
moonstal1506 <moonstal1506@gmail.com>
2024-01-05 18:10:36 +0900

    TaskletStep - startLimit() / allowStartIfComplete()

--- src/main/java/io/springbatch/springbatchlecture/LimitAllowConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/LimitAllowConfiguration.java
@@ -6,19 +6,13 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-import java.util.List;
-
 @RequiredArgsConstructor
-//@Configuration
-public class TaskletConfiguration {
+@Configuration
+public class LimitAllowConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -26,7 +20,6 @@ public class TaskletConfiguration {
     @Bean
     public Job batchJob() { //SimpleJob(설계도)
         return jobBuilderFactory.get("batchJob")
-                .incrementer(new RunIdIncrementer())
                 .start(step1())
                 .next(step2())
                 .build(); //job에 2개의 step을 저장 -> jobLuncher가 실행
@@ -34,19 +27,26 @@ public class TaskletConfiguration {
 
     @Bean
     public Step step1() {
-        return stepBuilderFactory.get("step1")
+        return stepBuilderFactory.get("step2")
                 .tasklet((stepContribution, chunkContext) -> {
                             System.out.println("step1");
                             return RepeatStatus.FINISHED;
                         }
                 )
+                .allowStartIfComplete(true)//성공해도 계속 실행
                 .build();
     }
 
     @Bean
     public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet(new CustomTasklet())
+        return stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> {
+                            System.out.println("step2");
+                            throw new RuntimeException("step2 was failed");
+//                            return RepeatStatus.FINISHED;
+                        }
+                )
+                .startLimit(3)//개수만큼 실행
                 .build();
     }
 
