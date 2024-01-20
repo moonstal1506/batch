796a5f52d11acf9fb8d10a68de5a371b067453fb
moonstal1506 <moonstal1506@gmail.com>
2024-01-20 12:59:27 +0900

    Transition - on() / to() / stop(), fail(), end(), stopAndRestart()

--- src/main/java/io/springbatch/springbatchlecture/BatchStatusExitStatusConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/BatchStatusExitStatusConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class BatchStatusExitStatusConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
